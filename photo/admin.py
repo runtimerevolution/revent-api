cc9707381b54ec708898dd12969ca884cc3fd735
cvagarinho <c.vagarinho@runtime-revolution.com>
2022-04-14 09:39:17 +0100

    changed models and admin

--- photo/admin.py
+++ photo/admin.py
@@ -1,3 +1,33 @@
+from ast import Sub
 from django.contrib import admin
+from .models import User, Contest, Submission, Vote, Comment, Result
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contest)
+class ContestAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    pass

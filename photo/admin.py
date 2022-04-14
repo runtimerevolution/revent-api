eee087c96110bb428f00c5308c1eebcb9641bbc9
cvagarinho <c.vagarinho@runtime-revolution.com>
2022-04-14 09:48:28 +0100

    added abstract to uuid model

--- photo/admin.py
+++ photo/admin.py
@@ -1,4 +1,3 @@
-from ast import Sub
 from django.contrib import admin
 from .models import User, Contest, Submission, Vote, Comment, Result
 
--- photo/models.py
+++ photo/models.py
@@ -6,29 +6,32 @@ import uuid
 class UUIDModel(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
 
+    class Meta:
+        abstract = True
 
-class User(models.Model):
+
+class User(UUIDModel):
     email = models.EmailField(("email address"), unique=True)
     date_joined = models.DateTimeField(("date joined"), default=timezone.now)
     first_name = models.TextField(("First Name"), blank=True, null=True)
     last_name = models.TextField(("Last Name"), blank=True, null=True)
 
 
-class Contest(models.Model):
+class Contest(UUIDModel):
     date_start = models.DateField(auto_now_add=True)
     date_end = models.DateField()
     name = models.TextField()
     description = models.TextField(blank=True, null=True)
 
 
-class Submission(models.Model):
+class Submission(UUIDModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
     content = models.TextField()  # unique=True?
     description = models.TextField(blank=True, null=True)  # max_length?
 
 
-class Comment(models.Model):
+class Comment(UUIDModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     submission = models.ForeignKey(
         Submission,
@@ -37,13 +40,13 @@ class Comment(models.Model):
     text = models.TextField()  # max_length?
 
 
-class Vote(models.Model):
+class Vote(UUIDModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
     value = models.IntegerField()
 
 
-class Result(models.Model):
+class Result(UUIDModel):
     contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
     position = models.PositiveIntegerField()

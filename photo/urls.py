8b1466f45aeb3271173eaeea355c12fb5cf3ccfb
andrevieira11 <andre.pa.vieira@hotmail.com>
2022-09-06 20:58:29 +0100

    fix: user detail url

--- photo/urls.py
+++ photo/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("submissions/", views.SubmissionList.as_view()),
     path("submissions/<uuid:id>/", views.SubmissionDetail.as_view()),
     path("users/", views.UserList.as_view()),
-    path("users/<uuid:id>/", views.UserDetail.as_view()),
+    path("users/<uuid:pk>/", views.UserDetail.as_view()),
     path("contests/", views.ContestList.as_view()),
     path("contests/<uuid:id>/", views.submissions_from_contest, name="submissions-from-contest"),
 ]

ec891728591eba9bc744cf3f31c01dcea54ea937
Miguel Rodrigues <miguel.rodrigues@runtime-revolution.com>
2022-09-07 12:02:21 +0100

    added modelviewsets and changed url's

--- photo/urls.py
+++ photo/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from photo import views
 
 urlpatterns = [
-    path("submissions/", views.SubmissionList.as_view()),
-    path("submissions/<uuid:id>/", views.SubmissionDetail.as_view()),
-    path("users/", views.UserList.as_view()),
-    path("users/<uuid:id>/", views.UserDetail.as_view()),
-    path("contests/", views.ContestList.as_view()),
-    path("users/authenticated/", views.current_user),
+    # path("submissions/", views.SubmissionList.as_view()),
+    # path("submissions/<uuid:id>/", views.SubmissionDetail.as_view()),
+    # path("users/", views.UserList.as_view()),
+    # path("users/<uuid:id>/", views.UserDetail.as_view()),
+    # path("contests/", views.ContestList.as_view()),
+    # path("users/authenticated/", views.current_user),
 ]

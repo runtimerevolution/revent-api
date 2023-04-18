decee6f348ec19937a58cd587188123bd147e3be
Tiago Cardoso <tiago.cardoso@runtime-revolution.com>
2023-04-18 10:41:08 +0100

    Add models

--- photo/models.py
+++ photo/models.py
@@ -1,52 +1,100 @@
-from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
-import uuid
 
 
-class UUIDModel(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+# Create your models here.
+class User(AbstractBaseUser):
+    USERNAME_FIELD = "email"
 
-    class Meta:
-        abstract = True
+    email = models.TextField(primary_key=True)
+    name_first = models.TextField(blank=True, null=True)
+    name_last = models.TextField(blank=True, null=True)
+    profile_picture = models.ForeignKey(
+        "Picture",
+        on_delete=models.SET_NULL,
+        related_name="user_picture_path",
+        null=True,
+    )
+    profile_picture_updated_at = models.DateTimeField(null=True)
+    user_handle = models.TextField(unique=True)
 
-
-class User(UUIDModel):
-    email = models.EmailField(("email address"), unique=True)
-    date_joined = models.DateTimeField(("date joined"), default=timezone.now)
-    first_name = models.TextField(("First Name"))
-    last_name = models.TextField(("Last Name"))
-
-
-class Contest(UUIDModel):
-    date_start = models.DateTimeField(auto_now_add=True)
-    date_end = models.DateTimeField()
-    name = models.TextField()
-    description = models.TextField()
-
-
-class Submission(UUIDModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
-    content = models.TextField()
-    description = models.TextField()
+    def __str__(self) -> str:
+        return self.name_first + self.name_last
 
 
-class Comment(UUIDModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    submission = models.ForeignKey(
-        Submission,
+class Picture(models.Model):
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="picture_user"
+    )
+    picture_path = models.TextField(primary_key=True)
+    likes = models.ManyToManyField(User, related_name="picture_likes")
+
+
+class PictureComment(models.Model):
+    user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+    )
+    picture = models.ForeignKey(
+        "Picture",
         on_delete=models.CASCADE,
     )
     text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Vote(UUIDModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
-    value = models.IntegerField()
+class Collection(models.Model):
+    name = models.TextField(null=True)
+    user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+    )
+    pictures = models.ManyToManyField(Picture)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["name", "user"], name="collection_pk")
+        ]
 
 
-class Result(UUIDModel):
-    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
-    submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
-    position = models.PositiveIntegerField()
+class Contest(models.Model):
+    title = models.TextField(null=True)
+    description = models.TextField()
+    cover_picture = models.ForeignKey(
+        "Picture",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    prize = models.TextField(null=True)
+    automated_dates = models.BooleanField(default=True)
+    upload_phase_start = models.DateTimeField(auto_now_add=True)
+    upload_phase_end = models.DateTimeField(null=True)
+    voting_phase_end = models.DateTimeField(null=True)
+    active = models.BooleanField(default=True)
+    winners = models.ManyToManyField(User, related_name="contest_winners")
+    created_by = models.ForeignKey(
+        "User",
+        on_delete=models.SET_NULL,
+        related_name="contest_created_by",
+        null=True,
+    )
+
+
+class ContestSubmission(models.Model):
+    contest = models.ForeignKey(
+        "Contest",
+        on_delete=models.CASCADE,
+    )
+    picture = models.ForeignKey(
+        "Picture",
+        on_delete=models.CASCADE,
+    )
+    submissionDate = models.DateTimeField(auto_now_add=True)
+    votes = models.ManyToManyField(User)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=["picture_user", "contest"], name="single_submission_per_user"
+            ),
+        )

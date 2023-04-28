29d7bc08a6f5ac63e6df8cf3f481f09e53d3d774
Tiago Cardoso <tiago.cardoso@runtime-revolution.com>
2023-04-28 08:51:50 +0100

    Add postgres settings

--- revent/settings.py
+++ revent/settings.py
@@ -71,19 +71,17 @@ WSGI_APPLICATION = "revent.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        # 'ENGINE':os.environ.get("SQL_ENGINE"),
-        # 'NAME': os.environ.get('SQL_DATABASE'),
-        # 'USER': os.environ.get('SQL_USER'),
-        # 'PASSWORD': os.environ.get('SQL_PASSWORD'),
-        # 'HOST': os.environ.get('SQL_HOST'),
-        # 'PORT': os.environ.get('SQL_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB'), 
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD':  os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators

11ea0bb83a2e9df31703e1c60215b0343fa3623e
Tiago Cardoso <tiago.cardoso@runtime-revolution.com>
2023-06-01 17:19:02 +0100

    Fix connection error

--- revent/urls.py
+++ revent/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from strawberry.django.views import GraphQLView
 
 from photo.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", GraphQLView.as_view(schema=schema)),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema))),
 ]

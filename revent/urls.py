0c9e24c703222f2f0f1a3266295a63276f792a3b
Tiago Cardoso <tiago.cardoso@runtime-revolution.com>
2023-06-01 16:35:15 +0100

    Fix graql view

--- revent/urls.py
+++ revent/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from strawberry.django.views import AsyncGraphQLView
+from strawberry.django.views import GraphQLView
 
 from photo.schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql/", AsyncGraphQLView.as_view(schema=schema)),
+    path("graphql/", GraphQLView.as_view(schema=schema)),
 ]

39968e1e63def2bc9d246ea0302fc6619103f627
Tiago Cardoso <tiago.cardoso@runtime-revolution.com>
2023-06-05 10:10:47 +0100

    Fix cors error

--- revent/settings.py
+++ revent/settings.py
@@ -49,9 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "revent.urls"
 

caf47f712420705bd9320e0beb0951d72593fc8a
Claudio Vagarinho <c.vagarinho@runtime-revolution.com>
2023-09-26 09:34:52 +0100

    changes to fields in admin

--- photo/admin.py
+++ photo/admin.py
@@ -26,24 +26,31 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(Picture)
 class PictureAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("user", "picture_path")
 
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "user")
 
 
 @admin.register(Contest)
 class ContestAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "title",
+        "description",
+        "prize",
+        "upload_phase_start",
+        "upload_phase_end",
+        "voting_phase_end",
+    )
 
 
 @admin.register(PictureComment)
 class PictureCommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("user", "picture", "text", "created_at")
 
 
 @admin.register(ContestSubmission)
 class ContestSubmissionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("contest", "picture", "submission_date")

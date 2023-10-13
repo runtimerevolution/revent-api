9d3368638deaaa2895a881b11a2f2772e31bb734
Claudio Vagarinho <c.vagarinho@runtime-revolution.com>
2023-10-13 17:20:19 +0100

    add env sample

--- config/settings.py
+++ config/settings.py
@@ -66,10 +66,18 @@ MIDDLEWARE = [
 
 SOCIALACCOUNT_PROVIDERS = {
     "google": {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        "APP": {"client_id": "123", "secret": "456", "key": ""}
+        "APP": {
+            "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+            "secret": os.environ.get("GOOGLE_CLIENT_SECRET"),
+            "key": "",
+        },
+        "SCOPE": [
+            "profile",
+            "email",
+        ],
+        "AUTH_PARAMS": {
+            "access_type": "online",
+        },
     }
 }
 

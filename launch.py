5d4f39296860e1063fa524fa48214534f99a9d7c
Tiago Cardoso <tiago.cardoso@runtime-revolution.com>
2023-12-22 09:27:51 +0000

    update populate

--- launch.py
+++ launch.py
@@ -1,6 +1,8 @@
 import os
+from datetime import timedelta
 
 from django.core.wsgi import get_wsgi_application
+from django.utils import timezone
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
@@ -16,3 +18,23 @@ contests = ContestFactory.create_batch(5)
 
 for contest in contests:
     ContestSubmissionFactory.create_batch(5, contest=contest)
+
+time = timezone.now()
+voting = ContestFactory(
+    upload_phase_start=time - timedelta(days=3),
+)
+
+ContestSubmissionFactory.create_batch(10, contest=voting)
+
+voting.upload_phase_end = time - timedelta(days=2)
+voting.voting_phase_end = time + timedelta(days=1)
+voting.save()
+
+closed = ContestFactory(
+    upload_phase_start=time - timedelta(days=3),
+)
+
+ContestSubmissionFactory.create_batch(10, contest=closed)
+closed.upload_phase_end = time - timedelta(days=2)
+closed.voting_phase_end = time - timedelta(days=1)
+closed.save()

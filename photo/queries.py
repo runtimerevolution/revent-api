68abe63c1348207bf812ee2e86d1a745de213dce
Tiago Cardoso <tiago.cardoso@runtime-revolution.com>
2024-01-18 10:12:22 +0000

    Fix queries

--- photo/queries.py
+++ photo/queries.py
@@ -93,15 +93,14 @@ class Query:
     def contest_submissions(
         self, filters: Optional[ContestSubmissionFilter] = strawberry.UNSET
     ) -> List[ContestSubmissionType]:
-        queryset = []
-        if filters.draw and filters.contest:
-            contest = Contest.objects.filter(
-                id=filters.contest.id, internal_status=ContestInternalStates.DRAW
-            ).first()
-            contest_winners = [user.id for user in contest.winners.all()]
-            queryset = ContestSubmission.objects.filter(
-                picture__user__id__in=contest_winners
-            )
-        else:
-            queryset = ContestSubmission.objects.all()
+        queryset = ContestSubmission.objects.all()
+        if filters:
+            if filters.draw and filters.contest:
+                contest = Contest.objects.filter(
+                    id=filters.contest.id, internal_status=ContestInternalStates.DRAW
+                ).first()
+                contest_winners = [user.id for user in contest.winners.all()]
+                queryset = ContestSubmission.objects.filter(
+                    picture__user__id__in=contest_winners
+                )
         return strawberry_django.filters.apply(filters, queryset)

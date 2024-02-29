6a9a299c1ef9b7878467423c0c780b23e2c7686e
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2024-02-29 21:31:56 +0000

    Remove unnecessary resources

--- terraform/09_auto_scaling.tf
+++ terraform/09_auto_scaling.tf
@@ -1,12 +1,4 @@
 # Targets
-# resource "aws_appautoscaling_target" "revent_app_target" {
-#   max_capacity       = var.autoscale_max
-#   min_capacity       = var.autoscale_min
-#   resource_id        = "service/${aws_ecs_cluster.revent_development.name}/${aws_ecs_service.revent_app.name}"
-#   scalable_dimension = "ecs:service:DesiredCount"
-#   service_namespace  = "ecs"
-#   depends_on         = [aws_ecs_service.revent_app]
-# }
 resource "aws_appautoscaling_target" "revent_nginx_target" {
   max_capacity       = var.autoscale_max
   min_capacity       = var.autoscale_min
@@ -17,21 +9,6 @@ resource "aws_appautoscaling_target" "revent_nginx_target" {
 }
 
 # Policies
-# resource "aws_appautoscaling_policy" "revent_app_policy" {
-#   name               = "revent-app-scaling-policy"
-#   policy_type        = "TargetTrackingScaling"
-#   resource_id        = aws_appautoscaling_target.revent_app_target.resource_id
-#   scalable_dimension = aws_appautoscaling_target.revent_app_target.scalable_dimension
-#   service_namespace  = aws_appautoscaling_target.revent_app_target.service_namespace
-#   target_tracking_scaling_policy_configuration {
-#     predefined_metric_specification {
-#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
-#     }
-#     target_value       = 75
-#     scale_in_cooldown  = 300
-#     scale_out_cooldown = 300
-#   }
-# }
 resource "aws_appautoscaling_policy" "revent_nginx_policy" {
   name               = "revent-nginx-scaling-policy"
   policy_type        = "TargetTrackingScaling"

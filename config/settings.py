ea3e2d5984ac1e2f6b49be82b5b61a9d9ae7898b
João Silva <j.silva@runtime-revolution.com>
2024-09-25 11:38:54 +0100

    Remove STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -163,9 +163,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = "staticfiles/"
-STATIC_ROOT = "/efs/staticfiles/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
